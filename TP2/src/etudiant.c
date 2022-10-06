6a2b714a6787ffe52081dbbe98ef9839ae6126fc
Benoit.Jomain <91122047+Ben21lyon@users.noreply.github.com>
2022-10-06 17:34:54 +0200

    Add files via upload

--- TP2/src/etudiant.c
+++ TP2/src/etudiant.c
@@ -2,15 +2,15 @@
 #include <string.h>
 int main(){
     // Initialisation  tableau (5 informations à traiter donc 5 tableaux)
-    int etu = 5;
-    int i;
+    int etu = 5; // Nombre d'étudiants
+    // On initialise les différents tableaux contenant les différentes données
     char tabN[][20] = {"Exbrayat","Imbert","Nenach","Jomain","Lenet"};
     char tabP[][20] = {"Baptiste","Tristan","Mathis","Benoît","Flavy"};
     char tabR[][20] = {"jean jaures","guichard","croix rousse","Emile Zola","jean charles"};
     int tabProg[] = {12,11,14,15,17};
     int tabSys[] = {11,14,17,18,19};
-
-    for(i=0;i<etu;i++){
+    // On parcourt chaque tableau où l'indice correspond i correspond à un étudiant	
+    for(int i=0;i<etu;i++){
         printf("Je suis %s %s, j'habite rue %s et j'ai %d en ProgC et %d en Système d'exploitation. \n",tabP[i],tabN[i],tabR[i],tabProg[i],tabSys[i]);
     } 
 }
--- TP2/src/etudiant2.c
+++ TP2/src/etudiant2.c
@@ -2,11 +2,11 @@
 #include <string.h>
 int main()
 {
-
+    // On initialise les différentes structures qui vont contenir les paramètres de l'adresse d'un étudiant (rue,ville, numéro de la rue) ou encore ses notes aux différents modules.	
     struct adresse
     {
         char rue[30];
-        char ville[30];
+        char ville[30]; // On fixe la longueur du tableau à 30 et donc on limite le nombre de caractères à 30
         char Nrue[10];
     };
 
@@ -16,7 +16,7 @@ int main()
         int NoteSysExploitation;
     };
 
-    struct etudiant
+    struct etudiant	// Structure dans une structure qui va donc contenir toutes les onfprmations d'un étudiant
     {
         char nom[30];
         char prenom[30];
@@ -25,7 +25,8 @@ int main()
         short int notes;
     };
 
-    struct etudiant etudiant_cpe[110];
+    struct etudiant etudiant_cpe[110];	// On définit le nombre d'étudiants (110)
+    // On ajoute nos étudiants les informations de nos différents étudiants au bon endroit dans la structure
     strcpy(etudiant_cpe[0].prenom, "Baptiste");
     strcpy(etudiant_cpe[0].nom, "Exbrayat");
     strcpy(etudiant_cpe[0].adresse.Nrue, "22");
@@ -66,6 +67,7 @@ int main()
     etudiant_cpe[4].module.NoteprogC = 12;
     etudiant_cpe[4].module.NoteSysExploitation = 17;
     int i;
+    // On affiche les informations des différents étudiants avec la boucle for ou i coreespond à chaque étudiant.
     for (i = 0; i < 5; i++)
     {
         printf("Je suis %s %s, j'habite %s rue %s à %s ,j'ai eu en ProgC %d et j'ai obtenu en Système d'exploitation %d \n ", etudiant_cpe[i].prenom, etudiant_cpe[i].nom, etudiant_cpe[i].adresse.Nrue, etudiant_cpe[i].adresse.rue, etudiant_cpe[i].adresse.ville, etudiant_cpe[i].module.NoteprogC,etudiant_cpe[i].module.NoteSysExploitation);
